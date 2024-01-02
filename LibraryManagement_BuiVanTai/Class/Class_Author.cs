136f4f6a9f632097dc24c8c77f8e92532feec814
vuthienduong182 <vuthienduong182@gmail.com>
2024-01-02 22:23:06 +0700

    Add Class

---
+++ LibraryManagement_BuiVanTai/Class/Class_Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement_BuiVanTai.Class
+{
+    public class Class_Author
+    {
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorDOB { get; set; }
+        public string AuthorAddress { get; set; }
+        public string AuthorEmail { get; set; }
+
+        public Class_Author(string authorId, string authorName, string authorDOB, string authorAddress, string authorEmail)
+        {
+            AuthorId = authorId;
+            AuthorName = authorName;
+            AuthorDOB = authorDOB;
+            AuthorAddress = authorAddress;
+            AuthorEmail = authorEmail;
+        }
+    }
+}
---
+++ LibraryManagement_BuiVanTai/Class/Class_Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement_BuiVanTai.Class
+{
+    public class Class_Book
+    {
+        public int BookId { get; set; }
+        public string BookTitle { get; set; }
+        public string Remaining { get; set; }
+        public string BookType { get; set; }
+        public string PublisherId { get; set; }
+        public string AuthorId { get; set; }
+        public string Price { get; set; }
+
+        public Class_Book(int bookId, string bookTitle, string remaining, string bookType, string publisherId, string authorId, string price)
+        {
+            BookId = bookId;
+            BookTitle = bookTitle;
+            Remaining = remaining;
+            BookType = bookType;
+            PublisherId = publisherId;
+            AuthorId = authorId;
+            Price = price;
+        }
+    }
+}
