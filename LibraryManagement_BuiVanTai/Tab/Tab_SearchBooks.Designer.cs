49e966f55fca56928115cb056784e1b8a05067c1
buivantai2303 <buivantai2303@gmail.com>
2024-01-02 09:25:00 +0700

    09:24 - 01/01/2024 - Tai

--- LibraryManagement_BuiVanTai/Tab/Tab_SearchBooks.Designer.cs
+++ LibraryManagement_BuiVanTai/Tab/Tab_SearchBooks.Designer.cs
@@ -267,7 +267,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(60, 46);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(321, 37);
