8871da8afb862cef85d96aaa855a3429e75d8d73
buivantai2303 <buivantai2303@gmail.com>
2024-01-06 16:16:14 +0700

    Update Receipt Details

--- LibraryManagement_BuiVanTai/Database/Database_ImportReceiptDetails.cs
+++ LibraryManagement_BuiVanTai/Database/Database_ImportReceiptDetails.cs
@@ -26,14 +26,22 @@ namespace LibraryManagement_BuiVanTai.Database
         {
             database = new Database(servername, databasename);
         }
-        
+
 
         // Get data from table Databse_ImportReceiptDetails ==========================================================
-        public DataTable getTable()
+        public DataTable getTable(string ID)
         {
-            dataTable = new DataTable();
-            dataTable = database.getTable(TableName);
-            return dataTable;
+            string sqlCommand = $"SELECT {ClassDefineName.table_ImportReceiptDetails_BookID}, " +
+                               $"{ClassDefineName.table_ImportReceiptDetails_ImportAmount}, " +
+                               $"{ClassDefineName.table_Suppliers_SupplierName}, " +
+                               $"{ClassDefineName.table_ImportReceiptDetails_Price} " + // Assuming this is the correct column for StaffName
+                               $"FROM {ClassDefineName.table_ImportReceiptDetails_TableName} " +
+                               $"JOIN {ClassDefineName.table_Suppliers_TableName} " +
+                               $"ON {ClassDefineName.table_ImportReceiptDetails_TableName}.{ClassDefineName.table_ImportReceiptDetails_SupplierID} = " +
+                               $"{ClassDefineName.table_Suppliers_TableName}.{ClassDefineName.table_Suppliers_SupplierID} " +
+                               $"WHERE {ClassDefineName.table_ImportReceiptDetails_ImportID} = '{ID}'";
+                               
+            return database.GetDataTable(sqlCommand);
         }
 
 
